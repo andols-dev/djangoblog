f75b548245c48692808fa6425817c8cb0b1f09f0
Andreas Olsson <andreasolsson2020@outlook.com>
2022-03-23 13:43:17 +0100

    update success message

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=300)
@@ -14,7 +13,6 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("details", kwargs={"id": self.id})
     
-
     def __str__(self):
         return self.title
     class Meta:
@@ -24,9 +22,6 @@ class Comment(models.Model):
     content = models.CharField(verbose_name="comment", max_length=50)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
-    
 
     def __str__(self):
         return self.content    
